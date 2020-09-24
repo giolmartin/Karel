fe64ad17a56763ffa605210bbee6f0fa376e3d9c
Gio <giovanni.lmartin@gmail.com>
2020-09-23 22:16:52 -0500

    5x3MapAdded, Even-1x8-8x1CheckerWorking, NotEvenWorlds

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -15,14 +15,129 @@ public class CheckerboardKarel extends SuperKarel {
 	public void run() {
 		
 		
-	}
-	
-	
-	public void turnRight() {
-		turnLeft();
-		turnLeft();
-		turnLeft();	
+		
+		if(frontIsBlocked() == true) {
+			placeBeeperException();
+		}
+			
+		while (frontIsClear() == true ) {
+				
+				placeBeepers();
+				switchStreetWest();
+				
+				if (facingNorth() == false) {
+				placeBeepers();	
+				switchStreetEast();
+				}
+		}
 		
 	}
+	
+	/*
+	 * Places Beepers on one street moving East
+	 */
+	
+	public void placeBeepers(){
+		
+		
+		putBeeper();
+		
+		while(frontIsClear() == true) {
+			
+			move();
+			
+			if(frontIsClear() == true) {
+				
+				move();
+				putBeeper();
+			
+			}
+		}
+		
+	}
+	
+	/*
+	 * Places beeper moving West
+	 */
+	
+	public void placeBeepersOdd() {
+		
 
+		while(frontIsClear() == true) {
+		
+		move();
+		putBeeper();
+		
+			
+			if(frontIsClear() == true) {
+			
+			move();
+			
+			}
+		
+		}
+	}
+
+	/*
+	 * Changes street, moving North and facing West
+	 */
+	
+	public void switchStreetWest() {
+		
+		if (facingNorth() == false) {
+		turnLeft();
+		}
+		
+		if(frontIsClear() == true) {
+			move();
+			turnLeft();
+		}
+	}
+	
+	/*
+	 * Changes street, moving North and facing East
+	 */
+	public void switchStreetEast() {
+		
+		if (facingNorth() == false) {
+			turnRight();
+			}
+		if(frontIsClear() == true) {
+			move();
+			turnRight();
+		} 
+			
+		
+		
+	}
+	
+	/*
+	 * This is a resolution when there is only ONE avenue present. 
+	 */
+	
+	public void placeBeeperException(){
+		
+		if(leftIsClear() == true) {
+		
+			turnLeft();
+		}
+		
+		if (frontIsClear() == true) {
+			placeBeepers();
+		}
+			
+	}
+	public void placeBeeperAvenueException() {
+			turnLeft();
+			putBeeper();
+		
+	}
+	
+	public void switchStreetsException() {
+		
+		if (frontIsClear() == true) {
+			move();
+		}
+	}
+	
 }
